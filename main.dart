c3b784522fda6838814851ebabe90ee800631382
david.rodriguezz <david.rodriguezz@grupomutual.fi.cr>
2022-07-04 11:09:24 -0600

    Mixins

--- main.dart
+++ main.dart
@@ -1,33 +1,46 @@
-//import 'dart:math' as math;
+abstract class Animal {}
+
+abstract class Mamifero extends Animal {}
+
+abstract class Ave extends Animal {}
+
+abstract class Pez extends Animal {}
+
+abstract class Volar {
+  void volar() => print('Puedo volar');
+}
+
+abstract class Caminar {
+  void caminar() => print('Puedo caminar');
+}
+
+abstract class Nadar {
+  void nadar() => print('Puedo nadar');
+}
+
+class Delfin extends Mamifero with Nadar {}
+
+class Murcielago extends Mamifero with Nadar, Caminar {}
+
+class Gato extends Mamifero with Caminar {}
+
+class Paloma extends Ave with Caminar, Volar {}
+
+class Pato extends Ave with Nadar, Caminar, Volar {}
+
+class Tiburon extends Ave with Nadar {}
+
+class PezVolador extends Ave with Nadar, Volar {}
 
 void main() {
-  final iroman = new Heroe('Tony Stark');
-  final spiderman = new Heroe('Peter Parker');
-  final tanos = new Villano('Tanos');
+  final flipper = new Delfin();
+  flipper.nadar();
 
-  print(iroman.nombre);
-  print(spiderman.nombre);
-  print(tanos.maldad);
-}
+  final donal = new Pato();
+  donal.caminar();
+  donal.volar();
+  donal.nadar();
 
-abstract class Personaje {
-  String? poder;
-  String? nombre;
-
-  Personaje(this.nombre);
-
-  @override
-  String toString() {
-    return '$nombre - $poder';
-  }
-}
-
-class Heroe extends Personaje {
-  int valentia = 110;
-  Heroe(String nombre) : super(nombre);
-}
-
-class Villano extends Personaje {
-  int maldad = 100;
-  Villano(String nombre) : super(nombre);
+  final espantaTiburon = new Tiburon();
+  espantaTiburon.nadar();
 }
