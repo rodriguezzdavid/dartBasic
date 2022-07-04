48893e2d356f1cb850999f2518748852db0b2747
david.rodriguezz <david.rodriguezz@grupomutual.fi.cr>
2022-07-04 10:15:42 -0600

    Extends

--- main.dart
+++ main.dart
@@ -1,40 +1,33 @@
 //import 'dart:math' as math;
 
 void main() {
-  final perro = new Perro();
-  final gato = new Gato();
-  final pato = new Pato();
+  final iroman = new Heroe('Tony Stark');
+  final spiderman = new Heroe('Peter Parker');
+  final tanos = new Villano('Tanos');
 
-  sonidoAnimal(perro);
-  sonidoAnimal(gato);
-  sonidoAnimal(pato);
+  print(iroman.nombre);
+  print(spiderman.nombre);
+  print(tanos.maldad);
 }
 
-void sonidoAnimal(Animal animal) {
-  animal.emitirSonido();
+abstract class Personaje {
+  String? poder;
+  String? nombre;
+
+  Personaje(this.nombre);
+
+  @override
+  String toString() {
+    return '$nombre - $poder';
+  }
 }
 
-abstract class Animal {
-  int? patas;
-  Animal();
-
-  void emitirSonido();
+class Heroe extends Personaje {
+  int valentia = 110;
+  Heroe(String nombre) : super(nombre);
 }
 
-class Perro implements Animal {
-  int? patas;
-  void emitirSonido() => print('Guauuuu');
-}
-
-class Gato implements Animal {
-  int? patas;
-  int? cola;
-  void emitirSonido() => print('Miauu');
-}
-
-class Pato implements Animal {
-  int? patas;
-  int? alas;
-  int? pico;
-  void emitirSonido() => print('Kuuuack');
+class Villano extends Personaje {
+  int maldad = 100;
+  Villano(String nombre) : super(nombre);
 }
