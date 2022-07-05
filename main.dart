5cf44fe1f1de868d854df830bda47961018fb144
david.rodriguezz <david.rodriguezz@grupomutual.fi.cr>
2022-07-04 20:03:54 -0600

    Futures

--- main.dart
+++ main.dart
@@ -1,46 +1,13 @@
-abstract class Animal {}
-
-abstract class Mamifero extends Animal {}
-
-abstract class Ave extends Animal {}
-
-abstract class Pez extends Animal {}
-
-abstract class Volar {
-  void volar() => print('Puedo volar');
-}
-
-abstract class Caminar {
-  void caminar() => print('Puedo caminar');
-}
-
-abstract class Nadar {
-  void nadar() => print('Puedo nadar');
-}
-
-class Delfin extends Mamifero with Nadar {}
-
-class Murcielago extends Mamifero with Nadar, Caminar {}
-
-class Gato extends Mamifero with Caminar {}
-
-class Paloma extends Ave with Caminar, Volar {}
-
-class Pato extends Ave with Nadar, Caminar, Volar {}
-
-class Tiburon extends Ave with Nadar {}
-
-class PezVolador extends Ave with Nadar, Volar {}
-
 void main() {
-  final flipper = new Delfin();
-  flipper.nadar();
+  print('Inicio');
 
-  final donal = new Pato();
-  donal.caminar();
-  donal.volar();
-  donal.nadar();
+  httpGet('https:api.food.com/pizza').then((data) {
+    print(data.toLowerCase());
+  });
 
-  final espantaTiburon = new Tiburon();
-  espantaTiburon.nadar();
+  print('Fin');
+}
+
+Future<String> httpGet(String url) {
+  return Future.delayed(Duration(seconds: 2), () => 'Hola MUNDOOO - 2 seg');
 }
