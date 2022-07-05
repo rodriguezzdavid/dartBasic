57a526fda17b6e775236f1ecc02416181da0324d
david.rodriguezz <david.rodriguezz@grupomutual.fi.cr>
2022-07-04 21:09:54 -0600

    Async - Await

--- main.dart
+++ main.dart
@@ -1,13 +1,26 @@
-void main() {
+void main() async {
+  //#1
   print('Inicio');
+  // httpGet('https:api.food.com/pizza').then((data) {
+  //   print(data.toLowerCase());
+  // });
 
-  httpGet('https:api.food.com/pizza').then((data) {
-    print(data.toLowerCase());
-  });
+  //#2
+  final nombre = await getNombre('20 000 000');
+  getNombre('20').then(print);
+  print(nombre);
 
+  //#3
+  print(await httpGet('https://api.nasa.com/aliens'));
+
+  //#4
   print('Fin');
 }
 
+Future<String> getNombre(String id) async {
+  return '$id - Fernando';
+}
+
 Future<String> httpGet(String url) {
-  return Future.delayed(Duration(seconds: 2), () => 'Hola MUNDOOO - 2 seg');
+  return Future.delayed(Duration(seconds: 2), () => 'Hola MUNDOOO - 2 SEG');
 }
