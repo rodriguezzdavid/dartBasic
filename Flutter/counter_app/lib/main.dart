fb08669160db9f35ece1ec0393b970e183585074
david.rodriguezz <david.rodriguezz@grupomutual.fi.cr>
2022-07-06 22:53:01 -0600

    Separando la Logica

--- Flutter/counter_app/lib/main.dart
+++ Flutter/counter_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:counter_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,9 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Center(
-        child: Text('Hola Mundo'),
-      ),
-    );
+        debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
