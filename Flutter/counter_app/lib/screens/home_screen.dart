884595cce5f11074b2027a44f510253320463032
david.rodriguezz <david.rodriguezz@grupomutual.fi.cr>
2022-07-08 11:23:33 -0600

    Botón flotante - FloatingActionButton

--- Flutter/counter_app/lib/screens/home_screen.dart
+++ Flutter/counter_app/lib/screens/home_screen.dart
@@ -5,10 +5,38 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        backgroundColor: Colors.pinkAccent,
-        body: Center(
-          child: Text('Giro de Italia'),
-        ));
+    const fontSize30 = TextStyle(fontSize: 30);
+
+    return Scaffold(
+      backgroundColor: Colors.pinkAccent,
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        elevation: 10,
+        //backgroundColor: Colors.red.shade900,
+      ),
+      body: Center(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              Text(
+                'Giro de Italia',
+                style: fontSize30,
+              ),
+              Text(
+                'Tour de Francia',
+                style: fontSize30,
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.pedal_bike_rounded),
+        onPressed: () {
+          print('object');
+        },
+      ),
+    );
   }
 }
