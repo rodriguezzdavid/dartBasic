22595308af666196393500612e14e9791a48f983
david.rodriguezz <david.rodriguezz@grupomutual.fi.cr>
2022-08-13 16:45:51 -0600

    Navegar a una nueva pantalla

--- Flutter/fl_components/lib/screens/home_screen.dart
+++ Flutter/fl_components/lib/screens/home_screen.dart
@@ -5,10 +5,18 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Components'),
+          elevation: 0,
+        ),
+        body: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+                  leading: const Icon(Icons.access_alarms_outlined),
+                  title: const Text('Nombre de ruta'),
+                  onTap: () {},
+                ),
+            separatorBuilder: (_, __) => const Divider(),
+            itemCount: 100));
   }
 }
