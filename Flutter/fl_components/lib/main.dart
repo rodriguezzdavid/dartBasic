232a3f19749318fc7e6531c141933920ec0d4786
david.rodriguezz <david.rodriguezz@grupomutual.fi.cr>
2022-08-13 16:06:17 -0600

    HomeScreen y rutas iniciales

--- Flutter/fl_components/lib/main.dart
+++ Flutter/fl_components/lib/main.dart
@@ -1,6 +1,6 @@
-//import 'package:fl_components/screens/listview1_screen.dart';
-import 'package:fl_components/screens/listview2_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'package:fl_components/screens/screens.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,10 +14,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'MATERIAL APP TOOL.KIT',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const Listview2Screen());
+        title: 'POKEMON APP',
+        initialRoute: 'home',
+        routes: {
+          'home': (BuildContext context) => const HomeScreen(),
+          'listview1': (BuildContext context) => const Listview1Screen(),
+          'listview2': (BuildContext context) => const Listview2Screen(),
+          'alert': (BuildContext context) => const AlertScreen(),
+          'card': (BuildContext context) => const CardScreen(),
+        });
   }
 }
--- Flutter/fl_components/lib/screens/listview2_screen.dart
+++ Flutter/fl_components/lib/screens/listview2_screen.dart
@@ -20,12 +20,18 @@ class Listview2Screen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Listview type #2'),
+          elevation: 0,
+          backgroundColor: Colors.green,
         ),
         body: ListView.separated(
           itemCount: options.length,
           itemBuilder: (context, i) => ListTile(
             title: Text(options[i]),
             trailing: const Icon(Icons.arrow_forward_ios_rounded),
+            onTap: () {
+              final animes = options[i];
+              print(animes);
+            },
           ),
           separatorBuilder: (_, __) => const Divider(),
         ));
